5cf053f66f8e8606c9660856b4d105fe563e8e1f
Rohan <rohan10hulk@gmal.com>
2019-11-15 18:35:19 +0530

    Updates

---
+++ precache-manifest.3edc7f6d602e8a9250cb91970ac624ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26daa073e128fecc7610b5bfcd1fa7c3",
+    "url": "/monster.rolodex/index.html"
+  },
+  {
+    "revision": "39d3093866bff5613644",
+    "url": "/monster.rolodex/static/css/main.4f6b94d7.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monster.rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "39d3093866bff5613644",
+    "url": "/monster.rolodex/static/js/main.32206a18.chunk.js"
+  },
+  {
+    "revision": "70266e25c5be1b976f3f",
+    "url": "/monster.rolodex/static/js/runtime-main.e861db85.js"
+  }
+]);
